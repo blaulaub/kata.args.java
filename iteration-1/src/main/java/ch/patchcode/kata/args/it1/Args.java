13c3bc616f02a9260d21f5edcbd6c32edaab2a29
Christian Fuchs <kontaktierbar@gmail.com>
2022-04-19 12:15:20 +0200

    extend test (count args)

--- iteration-1/src/main/java/ch/patchcode/kata/args/it1/Args.java
+++ iteration-1/src/main/java/ch/patchcode/kata/args/it1/Args.java
@@ -4,23 +4,31 @@ import java.util.Objects;
 
 public class Args {
 
+    private final int count;
+
     public static class UnexpectedArgumentException extends RuntimeException {
         public UnexpectedArgumentException(String arg) {
             super(arg);
         }
     }
 
+    public Args(int count) {
+        this.count = count;
+    }
+
     public static Args parse(String schema, String[] args) {
         Objects.requireNonNull(schema);
+        int count = 0;
         for (String arg: args) {
             if (!arg.equals("-" + schema)) {
                 throw new UnexpectedArgumentException(arg);
             }
+            count++;
         }
-        return new Args();
+        return new Args(count);
     }
 
     public int size() {
-        return 0;
+        return count;
     }
 }
--- iteration-1/src/test/java/ch/patchcode/kata/args/it1/SimpleArgumentsTest.java
+++ iteration-1/src/test/java/ch/patchcode/kata/args/it1/SimpleArgumentsTest.java
@@ -2,6 +2,8 @@ package ch.patchcode.kata.args.it1;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SimpleArgumentsTest {
 
     @Test
@@ -9,6 +11,7 @@ public class SimpleArgumentsTest {
         String schema = "h";
         String args[] = {"-h"};
         Args result = Args.parse(schema, args);
+        assertEquals(1, result.size());
     }
 
 }
