a05144c766c3c15b1a08c2422952f6c8667d5961
Christian Fuchs <kontaktierbar@gmail.com>
2022-04-19 14:24:28 +0200

    test two arguments

--- iteration-1/src/test/java/ch/patchcode/kata/args/it1/SimpleArgumentsTest.java
+++ iteration-1/src/test/java/ch/patchcode/kata/args/it1/SimpleArgumentsTest.java
@@ -24,4 +24,14 @@ public class SimpleArgumentsTest {
         assertNotNull(result.findArg("h"));
         assertNull(result.findArg("v"));
     }
+
+    @Test
+    void parseTwoSimpleArguments() {
+        String schema = "h|v";
+        String args[] = {"-h", "-v"};
+        Args result = new ArgsBuilder(schema).parse(args).build();
+        assertEquals(2, result.size());
+        assertNotNull(result.findArg("h"));
+        assertNotNull(result.findArg("v"));
+    }
 }
