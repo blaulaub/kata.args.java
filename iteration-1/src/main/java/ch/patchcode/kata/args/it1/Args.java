081ad0c40cc94b3a3f4fff1df24c9a6793b71dd5
Christian Fuchs <kontaktierbar@gmail.com>
2022-04-19 11:23:03 +0200

    added Args class

---
+++ iteration-1/src/main/java/ch/patchcode/kata/args/it1/Args.java
@@ -0,0 +1,6 @@
+package ch.patchcode.kata.args.it1;
+
+public class Args {
+    public static void parse(String schema, String[] args) {
+    }
+}
--- iteration-1/src/test/java/ch/patchcode/kata/args/it1/FirstTest.java
+++ iteration-1/src/test/java/ch/patchcode/kata/args/it1/FirstTest.java
@@ -3,8 +3,11 @@ package ch.patchcode.kata.args.it1;
 import org.junit.jupiter.api.Test;
 
 public class FirstTest {
+
     @Test
     void test() {
-
+        String schema = "h";
+        String args[] = {"-h"};
+        Args.parse(schema, args);
     }
 }
